1028683b36a718a2800697ba58f900f8c3337f0f
Ix <ixtreon@gmail.com>
2015-10-21 11:42:00 +0100

    Added a missing ShanoClient factory class to the `Client` project.

---
+++ Game/Client/ShanoGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public static class ShanoGame
+    {
+        public static IShanoClient Create(string playerName)
+        {
+            return new MainGame(playerName);
+        }
+    }
+}
