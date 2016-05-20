e42395993415e4632b48d73da07e21afbe835cc3
Ix <ixtreon@gmail.com>
2016-05-20 14:09:41 +0100

    Removed a redundant resource file from the Launcher.

--- Launcher/Properties/Resources.Designer.cs
+++ Launcher/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Shanism.Launcher.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap hero {
-            get {
-                object obj = ResourceManager.GetObject("hero", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
