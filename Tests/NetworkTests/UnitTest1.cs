2b4ee7b28ee6e8dfe499aa3b84386a80f69b01b9
Ix <ixtreon@gmail.com>
2017-01-15 03:28:13 +0000

    Fix up networking base unit test

--- Tests/NetworkTests/UnitTest1.cs
+++ Tests/NetworkTests/UnitTest1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Lidgren.Network;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shanism.Common;
 using Shanism.Common.Interfaces.Objects;
 using Shanism.Engine.Objects.Buffs;
 using Shanism.Network;
@@ -24,11 +25,14 @@ namespace NetworkTests
             var writer = new ClientStateTracker();
             var reader = new ObjectCache();
 
-            writer.WriteFrame(msg, 0, objs);
+            writer.WriteFrame(msg, 1, objs);
             msg.Position = 0;
             reader.ReadFrame(msg);
 
-            var bOut = reader.VisibleEntities.Single();
+            var bOut = reader.VisibleEntities.SingleOrDefault() as IBuff;
+            Assert.AreEqual(bOut.Attributes[HeroAttribute.Agility], 42);
+            Assert.AreEqual(bOut.Attributes[HeroAttribute.Intellect], 13);
+            Assert.AreEqual(bOut.Attributes[HeroAttribute.Strength], 5);
         }
 
 
